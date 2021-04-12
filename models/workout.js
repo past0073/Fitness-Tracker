d7550356d591522300ecf14fb17294400e474959
past0073 <past0073@umn.edu>
2021-04-11 22:19:02 -0500

    Created workout model.

---
+++ models/workout.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    type: {
+        type: String,
+        trim: true,
+        required: "Please enter a workout type"
+    },
+    name: {
+        tyoe: String,
+        trim: true,
+        required: "Please enter a workout name"
+    },
+    duration: {
+        type: Number,
+        required: "Please enter a workout duration"
+    },
+    distance: {
+        type: Number,
+        trim: true
+    },
+    weight: {
+        type: Number,
+        trim: true
+    },
+    sets: {
+        type: Number,
+        trim: true
+    },
+    reps: {
+        type: Number,
+        trim: true
+    }
+});
+
+db.workout.aggregate([
+  {
+      $addFields: {
+          totalSets: { $sum: "$sets" },
+          totalReps: { $sum: "$reps" }
+      },
+      $addFields: {
+          totalWeight: { $multiply: ["$weight", "$totalSets", "$totalReps"] }
+      },
+      $addFields: {
+        totalDistance: { $sum: "$distance"}
+      }
+  }
+ ])
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
