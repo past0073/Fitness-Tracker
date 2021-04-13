516b1a410db8f5e6c65885d5d2f9bc8665c89c8e
past0073 <past0073@umn.edu>
2021-04-12 20:37:46 -0500

    server.js created.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const PORT = proces.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.use(require("./routes/apiRoutes.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
